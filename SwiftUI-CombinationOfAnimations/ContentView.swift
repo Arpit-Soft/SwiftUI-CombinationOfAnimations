0a8e432de921f8592cd0ab2a8007efb2001f3abb
Arpit Dixit <arpitd@dewsolutions.in>
2021-08-05 19:21:12 +0530

    Added rotation Funky animations

--- SwiftUI-CombinationOfAnimations/ContentView.swift
+++ SwiftUI-CombinationOfAnimations/ContentView.swift
@@ -8,9 +8,25 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var showAnimations = false
+    
     var body: some View {
-        Text("Hello, world!")
+        VStack {
+            Spacer()
+            Text("🐦")
+                .font(.custom("Arial", size: 120))
+                .scaleEffect(showAnimations ? 2 : 1)
+                .rotationEffect(showAnimations ? .degrees(360) : .zero)
+                .animation(.interpolatingSpring(mass: 1.0, stiffness: 100.0, damping: 10.0, initialVelocity: 0))
+            Spacer()
+            Button("Click me") {
+                showAnimations.toggle()
+            }
             .padding()
+            .foregroundColor(.white)
+            .background(Color.green)
+        }
     }
 }
 
